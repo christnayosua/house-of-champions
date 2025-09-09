153131d6df6492ac717c60a581f15ba1cff746b8
Christna Yosua Rotinsulu <christna.yosua@ui.ac.id>
2025-09-09 22:22:12 +0700

    Melakukan penambahan attribute sesuai dengan kebutuhan di models.py, termasuk dengan attribute wajib sesuai dengan yang ada di panduan

--- main/models.py
+++ main/models.py
@@ -12,14 +12,21 @@ class News(models.Model):
         ('analysis', 'Analysis'),
     ]
     
+    # Item wajib
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
-    news_views = models.PositiveIntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='other')
     is_featured = models.BooleanField(default=False)
+    
+    # Attribute tambahan (opsional) -> butuh untuk informasi deskripsi item lebih lanjut
+    # Selain itu, rating berguna agar dapat menampilkan barang-barang yang direkomendasikan oleh aplikasi berdasarkan review user lain
+    stock = models.PositiveIntegerField(default=0)
+    rating = models.FloatField(default=0.0)
+    brand = models.CharField(max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.title
