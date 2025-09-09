e2ad273f9a06094a933a3a36103989412455e45b
Christna Yosua Rotinsulu <christna.yosua@ui.ac.id>
2025-09-09 22:40:31 +0700

    Melakukan perubahan bagian context di views.py sesuai dengan panduan serta mengubah struktur program dari main.html

--- main/views.py
+++ main/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'nameItem' : 'Messi',
-        'price': '$10.000',
-        'year': '2022 World Cup'
+        'applicationName' : 'House Of Champions',
+        'name': 'Christna Yosua Rotinsulu',
+        'class': 'PBP A',
+        'npm' : '2406495691',
     }
 
     return render(request, "main.html", context)
